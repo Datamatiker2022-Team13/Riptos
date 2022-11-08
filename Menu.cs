842f6c6e2b0e471634cca344253dea330b036b32
StephanieGaarsmand <113356066+StephanieGaarsmand@users.noreply.github.com>
2022-11-08 13:37:18 +0100

    start of Menu

---
+++ Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riptos
+{
+    public class Menu
+    {
+        public string Title { get; set; }
+        private List<MenuItem> menuItems;
+        public Menu(string title, int size)
+        {
+            Title = title;
+            menuItems = new List<MenuItem>();
+        }
+        public void Show()
+        {
+            Console.WriteLine(Title + "\n");
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                Console.WriteLine((i + 1) + "TEKST: " + menuItems[i].title);
+            }
+        }
+        public void AddItem(string menuTitle)
+        {
+            MenuItem mi = new MenuItem(menuTitle);
+            menuItems.Add(mi);  
+        }
+    }
+}
