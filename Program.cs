af1f4a48eab2f25564ff03151cd1ed841b239af3
Mathias <gamingmcseries@gmail.com>
2022-11-13 21:07:56 +0100

    + Added (EmployeeMenu) method + Added (HRMenu) method

--- Program.cs
+++ Program.cs
@@ -11,15 +11,25 @@
                 MainMenu.AddItem("Login");
                 MainMenu.AddItem("Glemt password?");
                 MainMenu.AddItem("Register ny bruger");
-                MainMenu.AddItem("Luk konsol");
+                MainMenu.AddItem("Luk konsol\n");
+
+                MainMenu.AddItem("[TEMP] Vis medarbejder menu");
+                MainMenu.AddItem("[TEMP] Vis HR-Medarbejder menu");
+
 
                 switch (MainMenu.GetSelection("Vælg handling: "))
                 {
                     case 0:
+                        Console.Clear();
+                        Console.WriteLine("Login");
                         break;
-                    case 1: 
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Glemt password");
                         break;
                     case 2:
+                        Console.Clear();
+                        Console.WriteLine("Register ny bruger");
                         break;
                     case 3:
                         Console.Clear();
@@ -37,7 +47,85 @@
 
                         running = false;
                         break;
+
+                    case 4:
+                        EmployeeMenu();
+                        break;
+                    case 5:
+                        HRMenu();
+                        break;
                 }
+            }
+        }
+        static void EmployeeMenu()
+        {
+            Console.Clear();
+
+            Menu Menu = new Menu("Velkommen til medarbejder siden");
+
+            Menu.AddItem("Vis kalender");
+            Menu.AddItem("Send henvendelse");
+            Menu.AddItem("Vis henvendelse");
+            Menu.AddItem("log ud");
+
+            switch (Menu.GetSelection("Vælg handling: "))
+            {
+                case 0:
+                    Console.WriteLine("Vis kalender");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Send hendvendelse");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Vis henvendelse");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Log ud");
+                    break;
+            }
+        }
+        static void HRMenu()
+        {
+            Console.Clear();
+
+            Menu Menu = new Menu("Velkommen til medarbejder siden");
+
+            Menu.AddItem("Vis kalender");
+            Menu.AddItem("Send henvendelse");
+            Menu.AddItem("Vis henvendelse");
+            Menu.AddItem("Opret sag");
+            Menu.AddItem("Vis sager");
+            Menu.AddItem("log ud");
+
+            switch (Menu.GetSelection("[HR] Vælg handling: "))
+            {
+                case 0:
+                    Console.WriteLine("[HR] Vis kalender");
+                    break;
+
+                case 1:
+                    Console.WriteLine("[HR] Send hendvendelse");
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("[HR] Vis henvendelse");
+                    break;
+
+                case 3:
+                    Console.WriteLine("[HR] Opret sag");
+                    break;
+
+                case 4:
+                    Console.WriteLine("[HR] Vis sager");
+                    break;
+
+                case 5:
+                    Console.WriteLine("[HR] Log ud");
+                    break;
             }
         }
     }
