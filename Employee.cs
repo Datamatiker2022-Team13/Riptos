36e10e1761ff3e55a72f67c41c6f103476003ea5
Casper <capper2704@hotmail.dk>
2022-11-15 13:25:46 +0100

    Early stage of the Employee class

--- Employee.cs
+++ Employee.cs
@@ -2,9 +2,36 @@
 {
     internal class Employee
     {
-       public string username;
+       private string username;
        private string password;
-       public string Name { get; set; }
-       public bool IsHR { get; set; }
+       private string name;
+       private bool isHR;
+
+       public string Username
+        {
+            get { return username; }
+        } 
+        public string Password
+        {
+            get { return password; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public bool IsHR
+        {
+            get { return isHR; }
+        }
+        public Employee (string name, bool isHR)
+        {
+            this.username = name;
+            this.isHR = isHR;
+        }
+        public void SetUserCredentials(string UserVal, string PassVal)
+        {
+            this.username = UserVal;
+            this.password = PassVal;
+        }
     }
 }
---
+++ EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riptos
+{
+    public class EmployeeRepository
+    {
+        string fileName = @"C:\Users\cappe\Desktop\Getting-Real\Riptos\LoginData.txt";
+
+        public void save(string pass)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileName, true))
+                {
+                    sw.WriteLine(pass);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public List<string> load()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    List<string> strings = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        strings.Add(line = sr.ReadLine());
+                    }
+                    return strings;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+
+            }
+        }
+    }
+}
