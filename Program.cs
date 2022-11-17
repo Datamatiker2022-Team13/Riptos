509c585e6195555f6ccd666d12adc265e024af38
KasperNissen1997 <KasperNissen1997@gmail.com>
2022-11-17 14:26:17 +0100

    Capper er smart

--- Program.cs
+++ Program.cs
@@ -3,11 +3,7 @@
     internal class Program
     {
         static void Main (string[] args) {
-            Console.WriteLine("");
-            EmployeeRepository ep = new EmployeeRepository();
-            ep.save("Banja",false,"Lort","SutterDiller");
-            string yeet = ep.load()[1].Name;
-            Console.WriteLine(yeet);
+            
         }
     } 
 }
