e06dbf457873d1d0e0275111e70782f4006651ee
KasperNissen1997 <KasperNissen1997@gmail.com>
2022-11-22 10:16:26 +0100

    Added EmployeeViewModel.

---
+++ Whistleblower/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Whistleblower.Models;
+
+namespace Whistleblower.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        /// <summary>
+        /// The original Employee object that is represented here throught the ViewModel.
+        /// </summary>
+        private Employee source;
+
+        public string Name { get; set; }
+        public bool IsHR { get; set; }
+
+        public string Username { get; set; }
+
+        public EmployeeViewModel (Employee source) {
+            this.source = source;
+
+            Name = source.Name;
+            IsHR = source.IsHR;
+
+            Username = source.Username;
+        }
+    }
+}
